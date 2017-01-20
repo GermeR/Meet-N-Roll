a1c2a03dd9d589b0ad4e3b49485acf86a78857e7
Robin Germe <robin.germe@etudiant.univ-lille1.fr>
2017-01-20 23:20:27 +0100

    new.html maggle

---
+++ src/web/servlet/ServletSingUp.java
@@ -0,0 +1,71 @@
+package web.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import web.struct.Personne;
+
+@WebServlet("/servlet/singup")
+public class ServletSingUp extends HttpServlet {
+
+	static final String NOM = "kwin";
+	static final String MDP = "moi";
+	static final String URL = "jdbc:postgresql://kwinserv.ddns.net:22042/MeetNRoll";
+
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse res)
+			throws ServletException, IOException {
+
+		HttpSession session = req.getSession();
+		PrintWriter out = res.getWriter();
+
+		Connection con = null;
+		Statement stmt = null;
+		ResultSet rs = null;
+		String sql = "SELECT * FROM personne WHERE login='"
+				+ req.getParameter("login") + "';";
+
+		if (req.getParameter("password").equals(req.getParameter("repassword"))) {
+
+			try {
+				Class.forName("org.postgresql.Driver");
+				con = DriverManager.getConnection(URL, NOM, MDP);
+				stmt = con.createStatement();
+				rs = stmt.executeQuery(sql);
+				if (rs.next()) {
+					session.setAttribute("personne",
+							new Personne(req.getParameter("login"), null, null,
+									null, null, null));
+				} else {
+					stmt.execute("insert into personne values('"
+							+ req.getParameter("login") + "','"
+							+ req.getParameter("password") + "');");
+				}
+				res.sendRedirect("../login.html");
+
+			} catch (ClassNotFoundException e) {
+				e.printStackTrace();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			} finally {
+				try {
+					con.close();
+				} catch (SQLException e) {
+				}
+			}
+		}else
+			res.sendRedirect("../new.html");
+	}
+}
