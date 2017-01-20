d3993e678f963916baa8a6656ea47737b1d45e21
Robin Germe <robin.germe@etudiant.univ-lille1.fr>
2017-01-20 22:40:14 +0100

    profil

--- src/web/servlet/ServletProfil.java
+++ src/web/servlet/ServletProfil.java
@@ -1,6 +1,7 @@
 package web.servlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -16,35 +17,45 @@ import javax.servlet.http.HttpSession;
 
 import web.struct.Personne;
 
-@WebServlet("/servlet/log")
-public class ServletLogin extends HttpServlet {
-	
+@WebServlet("/servlet/profil")
+public class ServletProfil extends HttpServlet {
+
 	static final String NOM = "kwin";
 	static final String MDP = "moi";
 	static final String URL = "jdbc:postgresql://kwinserv.ddns.net:22042/MeetNRoll";
 	
 	@Override
-	protected void service(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-
+	protected void service(HttpServletRequest req, HttpServletResponse res)
+			throws ServletException, IOException {
+		
+		PrintWriter out = res.getWriter();
 		HttpSession session = req.getSession();
-
+		
+		if(session.getAttribute("personne") == null)
+			res.sendRedirect("../login.html");
+		
+		Personne p = ((Personne)session.getAttribute("personne"));
+		
 		Connection con = null;
 		Statement stmt = null;
 		ResultSet rs = null;
-		String sql = "SELECT * FROM personne ;";
-		System.out.println(sql);
+		String sql = "SELECT * FROM joueurs where login='" + p.getLogin() + "';";
+
 		try {
 			Class.forName("org.postgresql.Driver");
 			con = DriverManager.getConnection(URL, NOM, MDP);
 			stmt = con.createStatement();
 			System.out.println(sql);
 			rs = stmt.executeQuery(sql);
+			out.println("<TABLE>");
+			out.println("<th>jdr favoris</th>");
 			while(rs.next())
-				System.out.println(rs.getString(1)+" "+rs.getString(2));
+				out.println("<tr><td>"+rs.getString(2)+"</td></tr>");
+			out.println("<TABLE>");
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-	}	
+	}
 }
