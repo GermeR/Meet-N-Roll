d6d05662c6072a6c6cacc0a61a7f48a2ea13d071
Robin Germe <robin.germe@etudiant.univ-lille1.fr>
2017-01-20 21:42:54 +0100

    comp.sh

--- comp.sh
+++ comp.sh
@@ -1,4 +1,2 @@
-for i in $(./ultraLs -fc src)
-do
-	javac -sourcepath src -cp ../../lib/servlet-api.jar:lib/postgresql-9.3-1102.jdbc41.jar -d WEB-INF/classes/ $i
-done
+javac -sourcepath src -cp ../../lib/servlet-api.jar:lib/postgresql-9.3-1102.jdbc41.jar -d WEB-INF/classes/ ./src/web/servlet/*
+javac -sourcepath src -cp ../../lib/servlet-api.jar:lib/postgresql-9.3-1102.jdbc41.jar -d WEB-INF/classes/ ./src/web/struct/*
